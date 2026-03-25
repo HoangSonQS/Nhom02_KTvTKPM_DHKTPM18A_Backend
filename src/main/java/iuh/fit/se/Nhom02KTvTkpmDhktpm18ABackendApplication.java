7538e4a8cb21910936dc200897c6c1941704577a
Hoang Son <sonjiny2004@gmail.com>
2026-03-25 22:13:22 +0700

    init project

---
+++ src/main/java/iuh/fit/se/Nhom02KTvTkpmDhktpm18ABackendApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom02KTvTkpmDhktpm18ABackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nhom02KTvTkpmDhktpm18ABackendApplication.class, args);
+    }
+
+}
